685481f9a282e2df0831d635bb1661e1e4263df6
racheli <racheli74621@gmail.com>
2025-03-11 00:43:36 +0200

    Your commit message here

--- src/components/leaving-course/leaving-course.component.ts
+++ src/components/leaving-course/leaving-course.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { RegistrationService } from '../../service/registration.service';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-leaving-course',
@@ -11,7 +12,7 @@ import { ActivatedRoute } from '@angular/router';
 export class LeavingCourseComponent {
  userId=sessionStorage.getItem('userId')||'';
   courseId!:string
-  constructor(private courseService: RegistrationService,private route: ActivatedRoute) { }
+  constructor(private courseService: RegistrationService,private route: ActivatedRoute,private router:Router) { }
   ngOnInit(): void {
     this.courseId = this.route.snapshot.paramMap.get('courseId')||'';
   
@@ -21,9 +22,22 @@ export class LeavingCourseComponent {
       .subscribe({
         next: (response) => {
           console.log('עזיבת הקורס הצליחה', response);
+           Swal.fire({
+                    title: '!הודעה',
+                    text: '.עזבת את הקורס בהצלחה',
+                    icon: 'success',
+                    confirmButtonText: 'אוקי'
+                  });
+                  this.router.navigate(['/courses'])
         },
         error: (error) => {
-          console.error('שגיאה בעזיבת הקורס', error);
+          Swal.fire({
+            title: '!שגיאה ',
+            text: 'עזיבת הקורס נכשלה',
+            icon: 'error',
+            confirmButtonText: 'אוקי'
+          });
+          this.router.navigate(['/courses'])
         }
       });
   }
